772187ab460e4c859091cc7e3f78ffd38fcd3830
Forairaaaaa <applesyqd@outlook.com>
2023-12-28 10:27:41 +0800

    .

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 sudo docker run --rm \
     --privileged \
     -v "${PWD}":/config \
-    --device=/dev/ttyUSB0 \
+    --device=/dev/ttyACM0 \
     -e "http_proxy=http://192.168.2.120:7890" \
     -e "https_proxy=http://192.168.2.120:7890" \
-    -it ghcr.io/esphome/esphome run ./echo_shit.yaml
+    -it ghcr.io/esphome/esphome run ./new_cores3_test.yaml
